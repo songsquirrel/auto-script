8394428dec460e28534bbf07c2f79ebf17f6dfa7
song <395135385@qq.com>
2025-03-21 10:50:40 +0800

    add nginx deploy by docker

---
+++ docker/web.sh
@@ -0,0 +1,26 @@
+# nginx
+mkdir -p /data/nginx/conf
+mkdir -p /data/nginx/log
+mkdir -p /data/nginx/html
+
+docker run --name nginx -d nginx:stable-perl
+
+docker cp nginx:/etc/nginx/nginx.conf /data/nginx/conf/nginx.conf
+docker cp nginx:/etc/nginx/conf.d /data/nginx/conf/conf.d
+docker cp nginx:/usr/share/nginx/html /data/nginx/html
+
+docker rm -f nginx
+
+docker run \
+-p 80:80 \
+--name nginx \
+-v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
+-v /data/nginx/conf/conf.d:/etc/nginx/conf.d \
+-v /data/nginx/log:/var/log/nginx \
+-v /data/nginx/html:/usr/share/nginx/html \
+-d nginx:stable-perl
+
+# postgres
+
+
+
